35c1d0fc1cb6db4e16804b7364ad245fe2576185
Laurent Etiemble <laurent.etiemble@gmail.com>
2024-06-05 19:10:40 +0200

    v0.0.2

--- lib/vagrant-tart/version.rb
+++ lib/vagrant-tart/version.rb
@@ -4,6 +4,6 @@ module VagrantPlugins
   # Top level module for the Tart provider plugin.
   module Tart
     # Current version of the Tart provider plugin.
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
